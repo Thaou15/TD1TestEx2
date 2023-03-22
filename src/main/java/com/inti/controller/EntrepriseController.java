da2e76649ce1d65623de22f17c174eef54218fa1
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-22 09:56:21 +0100

    Ajout de la fonction Save dans le controller Entreprise

--- src/main/java/com/inti/controller/EntrepriseController.java
+++ src/main/java/com/inti/controller/EntrepriseController.java
@@ -2,6 +2,11 @@ package com.inti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.inti.model.Entreprise;
 
 import com.inti.repository.IEntrepriseRepository;
 
@@ -10,6 +15,20 @@ public class EntrepriseController {
 	
 	@Autowired
 	IEntrepriseRepository ier;
+	
+	@GetMapping("ajoutE")
+	public String ajoutEntreprise()
+	{
+		return "formE";
+	}
+	
+	@PostMapping("save")
+	public String saveEntreprise(@ModelAttribute ("entreprise") Entreprise e)
+	{
+		 ier.save(e);
+			
+		return "redirect:/ajoutE";
+	}
 	
 
 }
